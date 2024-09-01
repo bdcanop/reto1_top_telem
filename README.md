d1d313f990f844645499022b1dd9d2d03ff8b5f2
Diego <damunerat@eafit.edu.co>
2024-09-01 12:03:23 -0500

    Agregando hilos y sockets para manejar el health check en nodos y superpeers

--- README.md
+++ README.md
@@ -1,16 +1,50 @@
-Cuando se inician las instancias que soportan los super peer Systemd busca todos los servicios habilitados y arranca el servicio llamado `flaskapp.service` automaticamente.
+# info de la materia: ST0263 <Topicos de Telematica>
+#
+# Estudiante(s): Brayan Daniel, email-eafit, Diego Alexander Munera Tobon, damunerat@eafit.edu.co
+#
+# Profesor: nombre, email-eafit
+#
+# <para borrar: EL OBJETIVO DE ESTA DOCUMENTACÍON ES QUE CUALQUIER LECTOR CON EL REPO, EN ESPECIAL EL PROFESOR, ENTIENDA EL ALCANCE DE LO DESARROLLADO Y QUE PUEDA REPRODUCIR SIN EL ESTUDIANTE EL AMBIENTE DE DESARROLLO Y EJECUTAR Y USAR LA APLICACIÓN SIN PROBLEMAS>
 
-Esto se hace porque los nodos hacen todas las peticiones a la siguiente URL:
-`http://internet-facing-1575917799.us-east-1.elb.amazonaws.com:8080/`
+# <para borrar: renombre este archivo a README.md cuando lo vaya a usar en un caso específico>
 
-Esto es el DNS del balanceador de cargas con el que cuenta la red. Allí se distribuye el registro de peers entre los super peers disponibles.
+# nombre del proyecto, lab o actividad
+#
+# 1. breve descripción de la actividad
+#
+<texto descriptivo>
+## 1.1. Que aspectos cumplió o desarrolló de la actividad propuesta por el profesor (requerimientos funcionales y no funcionales)
 
-Como los super peers estan dentro de una VPC y no cuentan con una dirección IP pública, se automatizó la ejecución del programa dentro de cada instancia con este script.
+## 1.2. Que aspectos NO cumplió o desarrolló de la actividad propuesta por el profesor (requerimientos funcionales y no funcionales)
 
-Si llegase a fallar se puede entrar a una de las instancias de super peer que actua como bastión la cual posee una dirección IP pública asignada mediante una IP elastica de AWS y se ejecutan los siguientes comandos:
+# 2. información general de diseño de alto nivel, arquitectura, patrones, mejores prácticas utilizadas.
 
-Para recargar Systemd: `sudo systemctl daemon-reload`
+# 3. Descripción del ambiente de desarrollo y técnico: lenguaje de programación, librerias, paquetes, etc, con sus numeros de versiones.
 
-Para iniciar el servicio de nuevo: `sudo systemctl start flaskapp.service`
+## como se compila y ejecuta.
+## detalles del desarrollo.
+## detalles técnicos
+## descripción y como se configura los parámetros del proyecto (ej: ip, puertos, conexión a bases de datos, variables de ambiente, parámetros, etc)
+## opcional - detalles de la organización del código por carpetas o descripción de algún archivo. (ESTRUCTURA DE DIRECTORIOS Y ARCHIVOS IMPORTANTE DEL PROYECTO, comando 'tree' de linux)
+## 
+## opcionalmente - si quiere mostrar resultados o pantallazos 
 
-Para comprobar el estado del servicio: `sudo systemctl status flaskapp.service`
+# 4. Descripción del ambiente de EJECUCIÓN (en producción) lenguaje de programación, librerias, paquetes, etc, con sus numeros de versiones.
+
+# IP o nombres de dominio en nube o en la máquina servidor.
+
+## descripción y como se configura los parámetros del proyecto (ej: ip, puertos, conexión a bases de datos, variables de ambiente, parámetros, etc)
+
+## como se lanza el servidor.
+
+## una mini guia de como un usuario utilizaría el software o la aplicación
+
+## opcionalmente - si quiere mostrar resultados o pantallazos 
+
+# 5. otra información que considere relevante para esta actividad.
+
+# referencias:
+<debemos siempre reconocer los créditos de partes del código que reutilizaremos, así como referencias a youtube, o referencias bibliográficas utilizadas para desarrollar el proyecto o la actividad>
+## sitio1-url 
+## sitio2-url
+## url de donde tomo info para desarrollar este proyecto
--- superpeer_1/app.py
+++ superpeer_1/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
-import requests, os, json
+import requests, os, json, threading, time
 
 app = Flask(__name__)
 
-nodes = {}
-superpeers = []
+nodes = {} # Nodos registrados en el superpeer
+known_superpeers = [] # Lista de superpeers conocidos
+active_superpeers = [] # Lista de superpeers activos
 
 # Cargar la configuración del super peer
 def load_config():
@@ -19,11 +20,36 @@ config = load_config()
     
 # Registrar los superpeers conocidos al iniciar la aplicación
 known_superpeers = config.get('known_superpeers', [])
-superpeers.extend(known_superpeers)
+active_superpeers.extend(known_superpeers)
 
 # Obtener host y puerto del superpeer
 host = config.get('host', '0.0.0.0')
 port = config.get('port', 8080)
+default_timeout = config.get('default_timeout', 2)
+
+# Función para verificar la disponibilidad de los superpeers
+def ping_superpeers():
+    while True:
+        for superpeer in known_superpeers[:]: # Esto es una copia de la lista
+            try:
+                response = requests.get(f"http://{superpeer}/health") # Endpoint de salud de los superpeers
+                if response.status_code == 200 and superpeer not in active_superpeers:
+                    active_superpeers.append(superpeer)
+                    print(f"Superpeer {superpeer} is back online and added to the network")
+            except requests.exceptions.RequestException:
+                if superpeer in active_superpeers:
+                    active_superpeers.remove(superpeer)
+                    print(f"Superpeer {superpeer} is offline and removed from the network")
+        time.sleep(default_timeout) # Espera 2 segundos antes de volver a verificar
+
+# Iniciar el hilo para verificar la disponibilidad de los superpeers
+ping_thread = threading.Thread(target=ping_superpeers, daemon=True)
+ping_thread.start()
+
+# Endpoint de salud para verificar la disponibilidad del superpeer
+@app.route('/health', methods=['GET'])
+def health_check():
+    return "OK", 200
 
 # Endpoint para registrar un nodo
 @app.route('/register', methods=['POST'])
@@ -35,24 +61,10 @@ def register_node():
         nodes[node_id] = resources
 
         print(f"Node {node_id} registered, nodes: {nodes}. #: {len(nodes)}")
-
-        return jsonify({"message": f"Node {node_id} registered successfully. Total nodes: {len(nodes)}"})
+        return jsonify({"message": f"Node {node_id} registered successfully. Total nodes: {len(nodes)}"}), 200
+    
     except Exception as e:
         print(f"Error registering node: {str(e)}")
-        return jsonify({"error": "Internal server error"}), 500
-
-# Endpoint para registrar un superpeer
-@app.route('/register-superpeer', methods=['POST'])
-def register_superpeer():
-    try:
-        data = request.json
-        superpeer_address = data['address']
-        if superpeer_address not in superpeers:
-            superpeers.append(superpeer_address)
-            print(f"Superpeer {superpeer_address} registered. Total superpeers: {len(superpeers)}")
-        return jsonify({"message": f"Superpeer {superpeer_address} registered successfully. Total superpeers: {len(superpeers)}"})
-    except Exception as e:
-        print(f"Error registering superpeer: {str(e)}")
         return jsonify({"error": "Internal server error"}), 500
 
 # Endpoint para buscar recursos
@@ -60,6 +72,7 @@ def register_superpeer():
 def search():
     try:
         resource_name = request.args.get('resource')
+
         if not resource_name:
             return jsonify({"error": "Resource parameter is required"}), 400
         
@@ -69,15 +82,15 @@ def search():
                 return jsonify({"node_id": node_id, "resource": resource_name})
         
         # Si no se encuentra localmente, buscar en otros superpeers
-        for superpeer in superpeers:
+        for superpeer in active_superpeers:
             try:
                 response = requests.get(f"http://{superpeer}/search", params={"resource": resource_name})
                 if response.status_code == 200:
                     return response.json()
             except Exception as e:
                 print(f"Error searching resource in superpeer {superpeer}: {str(e)}")
-        
-        return jsonify({"Message": "Resource not found"}), 404
+        return jsonify({"message": "Resource not found"}), 404
+    
     except Exception as e:
         print(f"Error searching resource: {str(e)}")
         return jsonify({"error": "Internal server error"}), 500
@@ -95,7 +108,7 @@ def list_nodes_and_resources():
 @app.route('/list-superpeers', methods=['GET'])
 def list_superpeers():
     try:
-        return jsonify(superpeers)
+        return jsonify(active_superpeers)
     except Exception as e:
         print(f"Error listing superpeers: {str(e)}")
         return jsonify({"error": "Internal server error"}), 500

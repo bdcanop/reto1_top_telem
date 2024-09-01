821a51d6be253cc94cc8183bd569a88ea5833b9f
Diego <damunerat@eafit.edu.co>
2024-09-01 15:16:51 -0500

    Removiendo super peers 2-4 porque no son necesarios para la ejecucion en aws

--- superpeer/app.py
+++ superpeer/app.py
@@ -11,7 +11,7 @@ logging.basicConfig(
     level = logging.INFO,
     format = '%(asctime)s - %(levelname)s - %(message)s',
     handlers = [
-        logging.FileHandler("superpeer2.log"),
+        logging.FileHandler("superpeer.log"),
         logging.StreamHandler()
     ]
 )
